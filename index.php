2934f9bd7c5a6c468f47be9c33ccd67b2c82f5bb
dionybusker <99057959@mydavinci.nl>
2020-02-21 10:02:38 +0100

    h1 toegevoegd, menu toegevoegd + styling

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>B3W22O1 - Mad libs</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Fontdiner+Swanky|Lakki+Reddy|Press+Start+2P&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <h1>Mad Libs</h1>
+        <div id="container">
+            <nav>
+                <ul id="menu">
+                    <li><a href="#">Er heerst paniek...</a></li>
+                    <li><a href="#">Onkunde</a></li>
+                </ul>
+            </nav>
+
+            <main>
+                <p>hi</p>
+            </main>
+        </div>
+    </body>
+</html>
