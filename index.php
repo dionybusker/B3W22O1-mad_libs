7160e625f138f44a94b8338014945009508a0a76
dionybusker <99057959@mydavinci.nl>
2020-02-21 11:24:39 +0100

    styling form

--- index.php
+++ index.php
@@ -7,8 +7,9 @@
         <link href="https://fonts.googleapis.com/css?family=Fontdiner+Swanky|Lakki+Reddy|Press+Start+2P&display=swap" rel="stylesheet">
     </head>
     <body>
+        <header><h1>Mad Libs</h1></header>
+
         <div id="container">
-            <h1>Mad Libs</h1>
 
             <nav>
                 <ul id="menu">
@@ -19,29 +20,39 @@
 
             <main>
                 <form action="paniekresult.php" method="post">
-                    <label for="question1">Welk dier zou je nooit als huisdier willen hebben?</label>
-                    <input id="question1" name="question1" type="text"><br>
+                    <div class="questions"><label for="question1">Welk dier zou je nooit als huisdier willen hebben?</label></div>
+                    <div class="questions-input"><input id="question1" name="question1" type="text"></div>
+                    <br>
 
-                    <label for="question2">Wie is de belangrijkste persoon in je leven?</label>
-                    <input id="question2" name="question2" type="text"><br>
+                    <div class="questions"><label for="question2">Wie is de belangrijkste persoon in je leven?</label></div>
+                    <div class="questions-input"><input id="question2" name="question2" type="text"></div>
+                    <br>
 
-                    <label for="question3">In welk land zou je graag willen wonen?</label>
-                    <input id="question3" name="question3" type="text"><br>
+                    <div class="questions"><label for="question3">In welk land zou je graag willen wonen?</label></div>
+                    <div class="questions-input"><input id="question3" name="question3" type="text"></div>
+                    <br>
 
-                    <label for="question4">Wat doe je als je je verveelt?</label>
-                    <input id="question4" name="question4" type="text"><br>
+                    <div class="questions"><label for="question4">Wat doe je als je je verveelt?</label></div>
+                    <div class="questions-input"><input id="question4" name="question4" type="text"></div>
+                    <br>
 
-                    <label for="question5">Met welk speelgoed speelde je als kind het meest?</label>
-                    <input id="question5" name="question5" type="text"><br>
+                    <div class="questions"><label for="question5">Met welk speelgoed speelde je als kind het meest?</label></div>
+                    <div class="questions-input"><input id="question5" name="question5" type="text"></div>
+                    <br>
 
-                    <label for="question6">Bij welke docent spijbel je het liefst?</label>
-                    <input id="question6" name="question6" type="text"><br>
+                    <div class="questions"><label for="question6">Bij welke docent spijbel je het liefst?</label></div>
+                    <div class="questions-input"><input id="question6" name="question6" type="text"></div>
+                    <br>
 
-                    <label for="question7">Als je € 100.000,- had, wat zou je dan kopen?</label>
-                    <input id="question7" name="question7" type="text"><br>
+                    <div class="questions"><label for="question7">Als je € 100.000,- had, wat zou je dan kopen?</label></div>
+                    <div class="questions-input"><input id="question7" name="question7" type="text"></div>
+                    <br>
 
-                    <label for="question8">Wat is je favoriete bezigheid?</label>
-                    <input id="question8" name="question8" type="text">
+                    <div class="questions"><label for="question8">Wat is je favoriete bezigheid?</label></div>
+                    <div class="questions-input"><input id="question8" name="question8" type="text"></div>
+
+                    <input type="submit" name="submit">
+                    
 
                 </form>
             </main>
